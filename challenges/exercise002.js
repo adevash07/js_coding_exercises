e7a576707f5c2f43f9f82561ac7516faf10d39a0
debugger <58630922+adevash07@users.noreply.github.com>
2023-01-19 20:54:42 +0100

    Exercise 3 complete

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -14,14 +14,12 @@ export function isFromManchester(person) {
 export function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
-  //return the number of buses required to transport the people
   return Math.ceil(people / 40);
 }
 
 export function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
-  //return the number of sheep in the array
   let count = 0;
   arr.forEach((item) => {
     if (item === "sheep") {
@@ -34,7 +32,6 @@ export function countSheep(arr) {
 export function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
   // Your code here!
-  //return true if the person lives in Manchester and has a postcode beginning with M
   if (person.address === undefined) return false;
   return (
     person.address.city === "Manchester" && person.address.postCode[0] === "M"
--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,26 +1,59 @@
 export function getSquares(nums) {
   if (nums === undefined) throw new Error("nums is required");
   // Your code here!
+  let squares = [];
+  nums.forEach((num) => {
+    squares.push(num * num);
+  });
+  return squares;
 }
 
 export function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
   // Your code here!
+  let camelCase = "";
+  words.forEach((word, index) => {
+    if (index === 0) {
+      camelCase += word;
+    } else {
+      camelCase += word[0].toUpperCase() + word.slice(1);
+    }
+  });
+  return camelCase;
 }
 
 export function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
   // Your code here!
+  let total = 0;
+  people.forEach((person) => {
+    total += person.subjects.length;
+  });
+  return total;
 }
 
 export function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
   // Your code here!
+  let found = false;
+  menu.forEach((item) => {
+    if (item.ingredients.includes(ingredient)) {
+      found = true;
+    }
+  });
+  return found;
 }
 
 export function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
   // Your code here!
+  let duplicates = [];
+  arr1.forEach((num) => {
+    if (arr2.includes(num) && !duplicates.includes(num)) {
+      duplicates.push(num);
+    }
+  });
+  return duplicates.sort();
 }
